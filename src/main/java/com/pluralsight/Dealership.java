ac9c22caed3460c9dbf3836439a836c5a8d88c54
Tunkz <Bubatunkz00@gmail.com>
2023-11-03 11:07:34 -0400

    fnished phase1

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,88 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<Vehicle>();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealership{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+     public ArrayList<Vehicle> getVehiclesByPrice(double min ,double max){
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel( String make, String model){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(String min, String max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(double min, double max){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles(){
+        return this.inventory;
+    }
+    public void addVehicle(Vehicle vehicle){
+    this.inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+
 }

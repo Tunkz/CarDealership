b1e71bbf083a4e9a4290d65e82fdf9205aa7fa21
Tunkz <Bubatunkz00@gmail.com>
2023-11-06 14:36:15 -0500

    Finished Dealership class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -50,40 +50,78 @@ public class Dealership {
                 ", phone='" + phone + '\'' +
                 '}';
     }
-     public List<Vehicle> getVehiclesByPrice(double min , double max){
-        return null;
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        List<Vehicle> listOfVehiclesByPrice = new ArrayList<Vehicle>();
+        for (Vehicle v : this.inventory) {
+            if (v.getPrice() >= min && v.getPrice() <= max) {
+                listOfVehiclesByPrice.add(v);
+            }
+
+        }
+        return listOfVehiclesByPrice;
     }
 
-
-    public List<Vehicle> getVehiclesByMakeModel( String make, String model){
-        return null;
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        List<Vehicle> listVehiclesByMakeModel = new ArrayList<Vehicle>();
+        for (Vehicle v : this.inventory){
+            if (v.getMake().equalsIgnoreCase(make) || v.getModel().equalsIgnoreCase(model)){
+                listVehiclesByMakeModel.add(v);
+            }
+        }
+        return listVehiclesByMakeModel;
     }
 
-    public List<Vehicle> getVehiclesByYear(String min, String max){
-        return null;
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        List<Vehicle> listOfVehiclesByYear = new ArrayList<Vehicle>();
+        for (Vehicle v : this.inventory) {
+            if (v.getYear() >= min && v.getYear() <= max) {
+                listOfVehiclesByYear.add(v);
+            }
+        }
+        return listOfVehiclesByYear;
     }
 
-    public List<Vehicle> getVehiclesByColor(String color){
-        return null;
+    public List<Vehicle> getVehiclesByColor(String color) {
+        List<Vehicle> listVehiclesByColor = new ArrayList<Vehicle>();
+        for (Vehicle c : this.inventory) {
+            if (c.getColor().equalsIgnoreCase(color)) {
+                listVehiclesByColor.add(c);
+            }
+        }
+        return listVehiclesByColor;
     }
 
-    public List<Vehicle> getVehiclesByMileage(double min, double max){
-        return null;
+    public List<Vehicle> getVehiclesByMileage(double min, double max) {
+        List<Vehicle> listOfVehiclesByMileage = new ArrayList<Vehicle>();
+        for (Vehicle v : this.inventory) {
+            if (v.getOdometer() >= min && v.getOdometer() <= max) {
+                listOfVehiclesByMileage.add(v);
+            }
+        }
+        return listOfVehiclesByMileage;
     }
 
-    public List<Vehicle> getVehiclesByType(String vehicleType){
-        return null;
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        List<Vehicle> listOfVehiclesByType = new ArrayList<Vehicle>();
+        for (Vehicle v : this.inventory) {
+            if (v.getVehicleType().equalsIgnoreCase(vehicleType)) {
+                listOfVehiclesByType.add(v);
+            }
+        }
+        return listOfVehiclesByType;
     }
 
-    public List<Vehicle> getAllVehicles(){
+    public List<Vehicle> getAllVehicles() {
         return this.inventory;
     }
-    public void addVehicle(Vehicle vehicle){
-    this.inventory.add(vehicle);
+
+    public void addVehicle(Vehicle vehicle) {
+        this.inventory.add(vehicle);
     }
 
-    public void removeVehicle(Vehicle vehicle){
-
+    public void removeVehicle(Vehicle vehicle) {
+        this.inventory.remove(vehicle);
     }
 
 }
